fadb68689288c87d3e97126f461bfda16da5ff97
rodnei0 <rodnei0@gmail.com>
2022-02-12 07:09:58 -0400

    feat/showproducts done

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import db from "../db.js";
 
 export async function getProducts (req, res) {
@@ -7,6 +8,21 @@ export async function getProducts (req, res) {
         const productsCollection = db.collection(process.env.MONGO_PRODUCTS);
         const products = await productsCollection.find({category: category}).toArray();
         
+		res.status(201).send(products);
+    } catch (error) {
+        res.status(500).send('A culpa foi do estagiário');
+        console.log(error);
+    }
+};
+
+export async function getBasketProducts (req, res) {
+    const { basket } = req.body;
+    const ids = basket.map(id => new ObjectId(id));
+    
+    try{
+        const productsCollection = db.collection(process.env.MONGO_PRODUCTS);
+        const products = await productsCollection.find({_id: {$in: ids}}).toArray();
+
 		res.status(201).send(products);
     } catch (error) {
         res.status(500).send('A culpa foi do estagiário');
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/productsController.js";
+import { getBasketProducts, getProducts } from "../controllers/productsController.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/products", getProducts);
+productsRouter.post("/basket", getBasketProducts);
 
 export default productsRouter;
