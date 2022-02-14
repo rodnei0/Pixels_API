8af8c5b9983c10df63505ae6d47c29a026a48185
Rayane Batista Ventura <rayyventura@gmail.com>
2022-02-14 09:28:59 -0300

    feat:lastfeats

--- src/controllers/historicController.js
+++ src/controllers/historicController.js
@@ -7,10 +7,24 @@ export default async function historicProducts(req,res){
             return sentStatus(401);
         }
     
-        const userData = await db.collection('historic').find({idUser:user._id}).toArray;
+        const userData = await db.collection('historic').find({idUser:user._id}).toArray();
         return res.send(userData);
     }catch(err){
         console.log(err);
         res.sendStatus(500);
     }
 }
+export async function purchasedItems(req,res){
+    const { items } = req.body;
+    const ids = items.map(id => new ObjectId(id));
+    
+    try{
+        const productsCollection = db.collection("products");
+        const products = await productsCollection.find({_id: {$in: ids}}).toArray();
+
+		res.status(201).send(products);
+    } catch (error) {
+        res.status(500).send('A culpa foi do estagiário');
+        console.log(error);
+    }
+    }
