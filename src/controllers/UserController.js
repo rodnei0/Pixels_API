b5b4266ad371b56b4ba04c3539f35c7b5c2b21c5
rodnei0 <rodnei0@gmail.com>
2022-02-14 12:48:23 -0400

    requirements implemented

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
 import db from "../db.js";
 
 export default async function signUp(req,res){
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,16 +2,17 @@ import { Router } from "express";
 import { validateSignUpSchema } from "../middleware/validateSignUpSchema.js";
 import signUp from "../controllers/UserController.js";
 import favoriteProducts, { addFavorites, deleteFavorite} from "../controllers/favoritesController.js";
-import  purchasedItems from "../controllers/historicController.js";
+import { purchasedItems, getPurchaseInfo } from "../controllers/historicController.js";
 import validateToken from '../middleware/validateToken.js'
 
 
 const userRouter = Router();
 
 userRouter.post('/signup', validateSignUpSchema,signUp);
-userRouter.get('/favorites',validateToken, favoriteProducts);
-userRouter.post('/favorites',validateToken, addFavorites);
-userRouter.delete('/favorites',validateToken, deleteFavorite);
-userRouter.post('/purchase',validateToken,purchasedItems);
+userRouter.get('/favorites', validateToken, favoriteProducts);
+userRouter.post('/favorites', validateToken, addFavorites);
+userRouter.delete('/favorites', validateToken, deleteFavorite);
+userRouter.get('/purchase', validateToken, getPurchaseInfo);
+userRouter.post('/purchase', validateToken, purchasedItems);
 
 export default userRouter;
